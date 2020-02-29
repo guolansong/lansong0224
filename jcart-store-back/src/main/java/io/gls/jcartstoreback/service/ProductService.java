a53e9c5afed60b510312965577a902b11e87c976
guolansong <2314627942.lan>
2020-02-29 17:20:32 +0800

    商店更新完毕

---
+++ jcart-store-back/src/main/java/io/gls/jcartstoreback/service/ProductService.java
@@ -0,0 +1,11 @@
+package io.gls.jcartstoreback.service;
+
+import com.github.pagehelper.Page;
+import io.gls.jcartstoreback.dto.out.ProductListOutDTO;
+import io.gls.jcartstoreback.dto.out.ProductShowOutDTO;
+
+public interface ProductService {
+    ProductShowOutDTO getById(Integer productId);
+
+    Page<ProductListOutDTO> search(Integer pageNum);
+}
