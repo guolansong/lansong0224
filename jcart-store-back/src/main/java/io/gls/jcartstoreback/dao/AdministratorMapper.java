398bee693c1f32447923d492d66e3e5a105bb8f6
guolansong <2314627942.lan>
2020-03-04 18:11:08 +0800

    fix store product mybatis files

---
+++ jcart-store-back/src/main/java/io/gls/jcartstoreback/dao/AdministratorMapper.java
@@ -0,0 +1,17 @@
+package io.gls.jcartstoreback.dao;
+
+import io.gls.jcartstoreback.po.Administrator;
+
+public interface AdministratorMapper {
+    int deleteByPrimaryKey(Integer administratorId);
+
+    int insert(Administrator record);
+
+    int insertSelective(Administrator record);
+
+    Administrator selectByPrimaryKey(Integer administratorId);
+
+    int updateByPrimaryKeySelective(Administrator record);
+
+    int updateByPrimaryKey(Administrator record);
+}
